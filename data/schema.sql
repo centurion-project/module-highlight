0606113ba40823db62f5bce18a97240ed72edd18
Florent Jaby <florent.jaby@gmail.com>
2012-07-12 18:59:27 +0200

    update schema

--- data/schema.sql
+++ data/schema.sql
@@ -1,41 +1,43 @@
 -- phpMyAdmin SQL Dump
--- version 3.3.2
+-- version 3.3.10deb1
 -- http://www.phpmyadmin.net
 --
--- Serveur: localhost
--- Généré le : Mer 16 Juin 2010 à 16:47
--- Version du serveur: 5.1.41
--- Version de PHP: 5.3.1
+-- Host: localhost
+-- Generation Time: Jul 12, 2012 at 06:58 PM
+-- Server version: 5.1.63
+-- PHP Version: 5.3.5-1ubuntu7.10
 
+SET FOREIGN_KEY_CHECKS=0;
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
-SET foreign_key_checks = 0;
+SET AUTOCOMMIT=0;
+START TRANSACTION;
 
 --
--- Base de données: `sam`
+-- Database: `afdel`
 --
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `highlight_container`
+-- Table structure for table `highlight_container`
 --
-drop table IF EXISTS highlight_container;
+
 CREATE TABLE IF NOT EXISTS `highlight_container` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) NOT NULL,
-  `feed_id` int(11) unsigned DEFAULT NULL,
-  `content_type_id` int(11) unsigned NOT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `proxy_content_type_id` int(11) unsigned DEFAULT NULL,
+  `proxy_pk` int(11) unsigned DEFAULT NULL,
   PRIMARY KEY (`id`),
-  KEY `hightlight_flux` (`feed_id`),
-  KEY `content_type_id` (`content_type_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+  KEY `proxy_model` (`proxy_content_type_id`,`proxy_pk`),
+  KEY `name` (`name`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=22 ;
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `highlight_feed`
+-- Table structure for table `highlight_feed`
 --
-drop table IF EXISTS highlight_feed;
+
 CREATE TABLE IF NOT EXISTS `highlight_feed` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `hightlight_container_id` int(11) unsigned DEFAULT NULL,
@@ -46,14 +48,14 @@ CREATE TABLE IF NOT EXISTS `highlight_feed` (
   PRIMARY KEY (`id`),
   KEY `hightlight_container_id` (`hightlight_container_id`,`proxy_content_type_id`),
   KEY `proxy_content_type_id` (`proxy_content_type_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 ;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `highlight_row`
+-- Table structure for table `highlight_row`
 --
-drop table IF EXISTS highlight_row;
+
 CREATE TABLE IF NOT EXISTS `highlight_row` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `proxy_content_type_id` int(11) unsigned DEFAULT NULL,
@@ -63,29 +65,25 @@ CREATE TABLE IF NOT EXISTS `highlight_row` (
   PRIMARY KEY (`id`),
   KEY `hightlight_container` (`container_id`),
   KEY `proxy_model` (`proxy_content_type_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=13 ;
 
 --
--- Contraintes pour les tables exportées
+-- Constraints for dumped tables
 --
 
 --
--- Contraintes pour la table `highlight_container`
---
-ALTER TABLE `highlight_container`
-  ADD CONSTRAINT `highlight_container_ibfk_1` FOREIGN KEY (`feed_id`) REFERENCES `highlight_feed` (`id`),
-  ADD CONSTRAINT `highlight_contenttype_ibfk_1` FOREIGN KEY (`content_type_id`) REFERENCES `centurion_content_type` (`id`);
-
---
--- Contraintes pour la table `highlight_feed`
+-- Constraints for table `highlight_feed`
 --
 ALTER TABLE `highlight_feed`
   ADD CONSTRAINT `highlight_feed_ibfk_1` FOREIGN KEY (`hightlight_container_id`) REFERENCES `highlight_container` (`id`),
   ADD CONSTRAINT `highlight_feed_ibfk_2` FOREIGN KEY (`proxy_content_type_id`) REFERENCES `centurion_content_type` (`id`);
 
 --
--- Contraintes pour la table `highlight_row`
+-- Constraints for table `highlight_row`
 --
 ALTER TABLE `highlight_row`
   ADD CONSTRAINT `highlight_row_ibfk_2` FOREIGN KEY (`container_id`) REFERENCES `highlight_container` (`id`),
   ADD CONSTRAINT `highlight_row_ibfk_3` FOREIGN KEY (`proxy_content_type_id`) REFERENCES `centurion_content_type` (`id`);
+SET FOREIGN_KEY_CHECKS=1;
+COMMIT;
+
