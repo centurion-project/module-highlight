13ada8df679ac43e3d5c78aa759b18550292cf9e
Florent Jaby <florent.jaby@gmail.com>
2012-07-12 16:06:54 +0200

    highlight view helper can get a container from name or proxy

--- views/helpers/GetHighlightContainer.php
+++ views/helpers/GetHighlightContainer.php
@@ -3,22 +3,22 @@
 class Highlight_View_Helper_GetHighlightContainer extends Zend_View_Helper_Abstract
 {
     /**
-     * returns the highlight container for the given name
-     * @todo find container from a proxy as well
+     * returns the highlight container for the given name or proxy
      */
-    public function getHighlightContainer($containerName)
+    public function getHighlightContainer($attached)
     {
         $table = Centurion_Db::getSingleton('highlight/container');
         
         Centurion_Cache_TagManager::addTag($table);
         Centurion_Cache_TagManager::addTag(Centurion_Db::getSingleton('highlight/row'));
         
-        $containerRow = Centurion_Db::getSingleton('highlight/container')->findOneByName($containerName);
-        
-        if ($containerRow === null) {
-            throw new Exception(sprintf('Container %s doesn\'t exists', $containerName));
+        if(is_string($attached)) {
+            $containerRow = Centurion_Db::getSingleton('highlight/container')->findOneByName($containerName);
+        }
+        else if($attached instanceof Centurion_Db_Table_Row_Abstract) {
+            $containerRow = Centurion_Db::getSingleton('highlight/container')->findOneByProxy($attached);
         }
         
-        return new Highlight_Model_DbTable_Rowset_Highlight(array('container' => $containerRow, 'name' => $containerName));
+        return $containerRow;
     }
 }
