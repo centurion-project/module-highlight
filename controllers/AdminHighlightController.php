9928bfc47bb1bfd9e625330506c0a4b6ef8ca74f
Florent Jaby <florent.jaby@gmail.com>
2012-07-17 15:49:26 +0200

    add aclcheck in highlight controller

--- controllers/AdminHighlightController.php
+++ controllers/AdminHighlightController.php
@@ -2,5 +2,12 @@
 
 class Highlight_AdminHighlightController extends Highlight_Model_Controller_AdminHighlightController
 {
+
+    public function init()
+    {
+        $this->_helper->authCheck();
+        $this->_helper->aclCheck();
+        parent::init();
+    }
     
 }
