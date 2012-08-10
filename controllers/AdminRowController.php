c60c50cb949480715dfc448f1f930a9d960ce2ca
Richard Déloge <richard@frenchcomp.net>
2012-08-10 18:38:11 +0300

    Update controllers/AdminRowController.php
    Fix bug : add "container_id" element in post action (with a new form, the action send to post action)
    But, if the element does not exist, the value is not put into the row)
    
    And use extraParam to keep the container into the request

--- controllers/AdminRowController.php
+++ controllers/AdminRowController.php
@@ -21,6 +21,8 @@ class Highlight_AdminRowController extends Centurion_Controller_CRUD //implement
         $this->view->placeholder('headling_1_content')->set($this->view->translate('Manage Highlight@backoffice,cms'));
         $this->view->placeholder('headling_1_add_button')->set($this->view->translate('highlight@backoffice,cms'));
         
+        //To conserv it into the request
+        $this->_extraParam['container'] = $this->getRequest()->getParam('container',0);
         parent::init();
     }
     
@@ -38,11 +40,19 @@ class Highlight_AdminRowController extends Centurion_Controller_CRUD //implement
         parent::newAction();
     }
 
+    public function postAction()
+    {
+        $container = $this->_getContainer();
+        $this->_getForm()->addElement('hidden', 'container_id', array('value'=>$container->id));
+
+        parent::postAction();
+    }
+
     protected function _getContainer()
     {
         if (!isset($this->_container)) {
                 $model = Centurion_Db::getSingleton('highlight/container');
-                $this->_container = $model->findOneById($this->_getParam('container', 0));
+                $this->_container = $model->findOneById($this->_extraParam['container']);
         }
         return $this->_container;
     }
