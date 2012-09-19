e4b89f1946a0d0c893e47a30b67808b51d5c58d8
Florent Jaby <florent.jaby@gmail.com>
2012-09-19 18:51:37 +0200

    alter table

--- data/20120919__add_fields.sql
+++ data/20120919__add_fields.sql
@@ -1,1 +1,2 @@
 ALTER TABLE  `highlight_row` CHANGE  `proxy_pk`  `proxy_pk` VARCHAR( 255 ) CHARACTER SET utf8  NULL DEFAULT NULL;
+ALTER TABLE  `highlight_container` ADD  `description` VARCHAR( 255 ) NULL DEFAULT NULL AFTER  `name`;
